a3136c768baeabafd6e6b9c09fe38fb8d95a0105
alex <alex.mack1995@gmail.com>
2020-10-27 12:06:10 -0700

    added buttons to tool bar, and added delete and add buttons. Moved the list to the main page

--- app/src/main/java/edu/asu/bsse/ajmack5/GeoLoc/MainActivity.java
+++ app/src/main/java/edu/asu/bsse/ajmack5/GeoLoc/MainActivity.java
@@ -4,37 +4,55 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import logic.JsonHandler;
 import logic.PlaceDescription;
 
 public class MainActivity extends AppCompatActivity {
+    JsonHandler jHandler;
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+    String path;
 
 
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = this;
+        String path = context.getFilesDir().toString();
         final Intent viewPlacesIntent = new Intent(this, placeListActivity.class);
         final Intent addPlaceIntent = new Intent(this, addPlaceActivity.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button viewPlaces = findViewById(R.id.viewPlaceList);
-        viewPlaces.setOnClickListener(new View.OnClickListener(){
-          public void onClick(View v){
+        getSupportActionBar().setTitle("GeoLoc"); // for set actionbar title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-              startActivity(viewPlacesIntent);
+        jHandler = new JsonHandler(path);
 
+        listView = (ListView) findViewById(R.id.placesListView);
+        List<String> namesOfPlaces = new ArrayList<String>();
+        for (int i = 0; i < jHandler.getPlaceDescriptionList().size(); i++) {
+            System.out.println("ADDING");
+            namesOfPlaces.add(jHandler.getPlaceDescriptionList().get(i).getName());
+        }
 
-            }
-        });
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namesOfPlaces);
+        listView.setAdapter(arrayAdapter);
+
 
         final Button addNewPlace = findViewById(R.id.addNewPlace);
-        addNewPlace.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        addNewPlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
                 startActivity(addPlaceIntent);
 
@@ -42,6 +60,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                System.out.println(o.toString());
+            }
+        });
+
+
+    }
 
 
 
@@ -59,5 +88,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-}
+
 
